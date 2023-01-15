13912c781f72aff2d757e2d0fc4aacfd38f53589
shueHeart <95758078+shueHeart@users.noreply.github.com>
2023-01-15 16:23:25 +0400

    Add files via upload
    add graph way

---
+++ graphway/SimpleGraph.java
@@ -0,0 +1,128 @@
+
+import java.util.*;
+
+class Vertex {
+	public int Value;
+	public boolean Hit;
+
+	public Vertex(int val) {
+		Value = val;
+		Hit = false;
+	}
+}
+
+class SimpleGraph {
+	public Vertex[] vertex;
+	public int[][] m_adjacency;
+	public int max_vertex;
+
+	public SimpleGraph(int size) {
+		max_vertex = size;
+		m_adjacency = new int[size][size];
+		vertex = new Vertex[size];
+	}
+
+	public void AddVertex(int value) {
+		
+		for (int i = 0; i < max_vertex; ++i) {
+			
+			if (vertex[i] == null) {
+				
+				vertex[i] = new Vertex(value);
+				break;
+				
+			}
+			
+		}
+		
+	}
+
+	public void RemoveVertex(int v) {
+		
+		if (vertex[v] == null) {
+			return;
+		}
+		
+		for (int i = 0; i < max_vertex; ++i) {
+			
+			m_adjacency[v][i] = 0;
+			m_adjacency[i][v] = 0;
+			
+		}
+		
+		vertex[v] = null;
+		
+	}
+
+	public boolean IsEdge(int v1, int v2) {
+		
+		if (vertex[v1] == null || vertex[v2] == null) {
+			return false;
+		}
+		
+		return m_adjacency[v1][v2] == 1;
+		
+	}
+
+	public void AddEdge(int v1, int v2) {
+		
+		if (vertex[v1] == null || vertex[v2] == null) {
+			return;
+		}
+		
+		m_adjacency[v1][v2] = 1;
+		
+	}
+
+	public void RemoveEdge(int v1, int v2) {
+		
+		if (vertex[v1] == null || vertex[v2] == null) {
+			return;
+		}
+		
+		m_adjacency[v1][v2] = 0;
+		
+	}
+
+	public ArrayList<Vertex> DepthFirstSearch(int VFrom, int VTo) {
+
+		ArrayList<Vertex> way = new ArrayList<Vertex>();
+
+		DepthFirstSearch(way, VFrom, VTo);
+
+		return way;
+
+	}
+
+	private void DepthFirstSearch(ArrayList<Vertex> way, int VNowIndex, int VTo) {
+
+		way.add(vertex[VNowIndex]);
+		
+		
+		for (int i = 0; i < max_vertex; ++i) {
+			
+
+			if (vertex[i] == null) {
+				continue;
+			}
+			
+			if (m_adjacency[VNowIndex][i] == 0) {
+				continue;
+			}
+			
+			if (i == VTo) {
+				way.add(vertex[i]);
+				return;
+			}
+			
+			DepthFirstSearch(way, i, VTo);
+			
+		}
+
+		if (way.get(way.size() - 1).Value != vertex[VTo].Value) {
+			way.remove(way.size() - 1);
+		}
+		
+	}
+
+}
